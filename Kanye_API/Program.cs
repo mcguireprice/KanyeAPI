1b83658af4bac2a1864e9103ea32eeabaefa023d
McGuire Price <mcguire02170@gmail.com>
2019-07-31 08:07:10 -0500

    Finished API

---
+++ Kanye_API/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RestSharp;
+namespace Kanye_API
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Here's a quote from Kanye West. " + GetReleases());
+		}
+		public static string GetReleases()
+		{
+			var client = new RestClient("https://api.kanye.rest");
+			var response = client.Execute(new RestRequest());
+			return response.Content.Substring(9).TrimEnd('}');
+		}
+	}
+}
